dd2cba7a44a394676156c1c1b41dcd394b786fe9
yiyiyilingjie <390200763@qq.com>
2020-02-12 14:47:47 +0800

    完成登录功能

--- README.md
+++ README.md
@@ -22,3 +22,18 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+1.导入mydb.sql文件，路径不能有中文
+
+2.导入mydb.sql文件时多等几分钟
+
+3.在Vue ui中出现编译错误，看输出
+
+4.tabs太严格，在.eslintric.js中的rules下面加一个'no-tabs': 'off'
+
+5.坑人的eslint，缩进只能用空格缩进（规范）代码最后需要留一行即回车一下，不必要的分号；要去掉，对象方法后面需要空一格,
+单引号来表示字符串
+
+6.對5部分問題的解決:'space-before-function-paren':0 ，方法後面可以不加空格了。
--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+// 配置请求的跟路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 这里给了Vue一个属性值为$http，这样vue实例对象就可以通过this.$http来获取axios请求的路径
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
