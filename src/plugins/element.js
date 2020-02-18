a122b95a056db58758c7ddc4d8ed0fa769b5970a
yiyiyilingjie <390200763@qq.com>
2020-02-18 17:35:12 +0800

    完成订单功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 import {
   Button,
   Form,
@@ -76,6 +78,9 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
